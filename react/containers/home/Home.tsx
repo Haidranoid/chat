3b2bae55a1bbb6df45d7a82e154366fb86d21693
Jose Eduardo Martinez Lara <martinezlara_joseeduardo@hotmail.com>
2020-09-25 10:34:09 -0500

    user loggin started

--- react/containers/home/Home.tsx
+++ react/containers/home/Home.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import useSocket from "../../hooks/use-socket/useSocket";
+import {useLocation} from "react-router-dom";
+import queryString from "query-string";
+
+interface user {
+    id: string,
+    name: string,
+    room: string,
+}
 
 const Home = () => {
+    const socketClient = useSocket();
+    const location = useLocation().search;
+    const query = queryString.parse(location);
+
+    if (!query.name) return <div>
+        Not name found
+    </div>;
+
+    useEffect(() => {
+        socketClient.emit('userLogged', {name: query.name}, (res: Array<user>) => {
+            console.log("Users connected: ", res)
+        })
+
+    }, []);
+
     return (
         <div>
-            Chat
+            Bienvenido {query.name}
         </div>
     );
 };
--- server/sockets/socketServer.js
+++ server/sockets/socketServer.js
@@ -7,6 +7,12 @@ socketServer.on("connection", client => {
     console.log("Client connected");
 
     client.on('disconnect', () => {
-        console.log("Client disconnected")
+        console.log("Client disconnected");
     });
+
+    client.on('userLogged', (user, callback) => {
+        console.log('User logged: ', user);
+        users.addUser(client.id, user.name);
+        callback(users.getUsers())
+    })
 });
